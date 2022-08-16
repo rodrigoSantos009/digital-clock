a78a44f8bebed6be089772c8affd60cb6f260023
Rodrigo <009rodrigo009@gmail.com>
2022-08-16 09:21:57 -0300

    atualização

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const hourElement = document.getElementById('hour')
 const minuteElement = document.getElementById('minutes')
 const secondElement = document.getElementById('seconds')
-const ampmElement = document.getElementById('ampm')
 
 function updateClock() {
     let hour = new Date().getHours()
     let minutes = new Date().getMinutes()
     let seconds = new Date().getSeconds()
-    let newFormat = hour >= 12 ? 'PM' : 'AM'
     
     hour = hour < 10 ? "0" + hour : hour
     minutes =  minutes < 10 ? "0" + minutes : minutes
@@ -16,7 +14,6 @@ function updateClock() {
     hourElement.innerHTML = hour
     minuteElement.innerHTML = minutes
     secondElement.innerHTML = seconds
-    ampmElement.innerHTML = newFormat
 
     setTimeout(() => {
         updateClock()
